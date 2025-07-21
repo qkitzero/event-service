8bdad81daa7b1eaa32370c86dcb86faa5c86b362
qkitzero <qkitzero@gmail.com>
2025-07-21 23:38:33 +0900

    add: repository test

---
+++ internal/infrastructure/event/repository_test.go
@@ -0,0 +1,95 @@
+package event
+
+import (
+	"errors"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+	"go.uber.org/mock/gomock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/qkitzero/event-service/internal/domain/event"
+	mocksevent "github.com/qkitzero/event-service/mocks/domain/event"
+	"github.com/qkitzero/event-service/testutil"
+)
+
+func TestCreate(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		success bool
+		setup   func(mock sqlmock.Sqlmock, event event.Event)
+	}{
+		{
+			name:    "success create event",
+			success: true,
+			setup: func(mock sqlmock.Sqlmock, event event.Event) {
+				mock.ExpectBegin()
+
+				mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO "events" ("id","title","description","created_at") VALUES ($1,$2,$3,$4)`)).
+					WithArgs(event.ID(), event.Title(), event.Description(), testutil.AnyTime{}).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+
+				mock.ExpectCommit()
+			},
+		},
+		{
+			name:    "failure create event error",
+			success: false,
+			setup: func(mock sqlmock.Sqlmock, event event.Event) {
+				mock.ExpectBegin()
+
+				mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO "events" ("id","title","description","created_at") VALUES ($1,$2,$3,$4)`)).
+					WithArgs(event.ID(), event.Title(), event.Description(), testutil.AnyTime{}).
+					WillReturnError(errors.New("create event error"))
+
+				mock.ExpectRollback()
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			sqlDB, mock, err := sqlmock.New()
+			if err != nil {
+				t.Errorf("failed to new sqlmock: %s", err)
+			}
+
+			gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: sqlDB}), &gorm.Config{})
+			if err != nil {
+				t.Errorf("failed to open gorm: %s", err)
+			}
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockEvent := mocksevent.NewMockEvent(ctrl)
+			mockEvent.EXPECT().ID().Return(event.EventID{UUID: uuid.New()}).AnyTimes()
+			mockEvent.EXPECT().Title().Return(event.Title("title")).AnyTimes()
+			mockEvent.EXPECT().Description().Return(event.Description("description")).AnyTimes()
+			mockEvent.EXPECT().CreatedAt().Return(time.Now()).AnyTimes()
+
+			tt.setup(mock, mockEvent)
+
+			repo := NewEventRepository(gormDB)
+
+			err = repo.Create(mockEvent)
+			if tt.success && err != nil {
+				t.Errorf("expected no error, but got %v", err)
+			}
+			if !tt.success && err == nil {
+				t.Errorf("expected error, but got nil")
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
