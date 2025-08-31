d66dad4c4846ade71b3eedaf5f3213d2ca25f8e7
qkitzero <qkitzero@gmail.com>
2025-08-31 16:06:15 +0900

    add: list events test

--- internal/application/event/usecase_test.go
+++ internal/application/event/usecase_test.go
@@ -7,6 +7,7 @@ import (
 
 	"go.uber.org/mock/gomock"
 
+	"github.com/qkitzero/event-service/internal/domain/event"
 	mocks "github.com/qkitzero/event-service/mocks/domain/event"
 )
 
@@ -42,6 +43,43 @@ func TestCreateEvent(t *testing.T) {
 			eventUsecase := NewEventUsecase(mockEventRepository)
 
 			_, err := eventUsecase.CreateEvent(tt.userID, tt.title, tt.description, tt.startTime, tt.endTime)
+			if tt.success && err != nil {
+				t.Errorf("expected no error, but got %v", err)
+			}
+			if !tt.success && err == nil {
+				t.Errorf("expected error, but got nil")
+			}
+		})
+	}
+}
+
+func TestListEvents(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name            string
+		success         bool
+		userID          string
+		listByUserIDErr error
+	}{
+		{"success list events", true, "6d322c66-bf4d-427a-970c-874f3745f653", nil},
+		{"failure empty user id", false, "", nil},
+		{"failure list by user id error", false, "6d322c66-bf4d-427a-970c-874f3745f653", errors.New("list by user id error")},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockEvent := mocks.NewMockEvent(ctrl)
+			mockEventRepository := mocks.NewMockEventRepository(ctrl)
+			mockEventRepository.EXPECT().ListByUserID(gomock.Any()).Return([]event.Event{mockEvent}, tt.listByUserIDErr).AnyTimes()
+
+			eventUsecase := NewEventUsecase(mockEventRepository)
+
+			_, err := eventUsecase.ListEvents(tt.userID)
 			if tt.success && err != nil {
 				t.Errorf("expected no error, but got %v", err)
 			}
