6864112b5f090f07cb1e9c53c49c03b2396ea3fe
qkitzero <qkitzero@gmail.com>
2025-10-12 22:21:11 +0900

    add: find by id test

--- internal/infrastructure/event/repository_test.go
+++ internal/infrastructure/event/repository_test.go
@@ -180,6 +180,84 @@ func TestUpdate(t *testing.T) {
 	}
 }
 
+func TestFindByID(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		success bool
+		id      event.EventID
+		setup   func(mock sqlmock.Sqlmock, id event.EventID)
+	}{
+		{
+			name:    "success find by id",
+			success: true,
+			id:      event.EventID{UUID: uuid.New()},
+			setup: func(mock sqlmock.Sqlmock, id event.EventID) {
+				eventRows := sqlmock.NewRows([]string{"id", "user_id", "title", "description", "start_time", "end_time", "created_at", "updated_at"}).
+					AddRow(id, uuid.New(), "title", "description", time.Now(), time.Now(), time.Now(), time.Now())
+				mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "events" WHERE id = $1 ORDER BY "events"."id" LIMIT $2`)).
+					WithArgs(id, 1).
+					WillReturnRows(eventRows)
+			},
+		},
+		{
+			name:    "failure event not found",
+			success: false,
+			id:      event.EventID{UUID: uuid.New()},
+			setup: func(mock sqlmock.Sqlmock, id event.EventID) {
+				mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "events" WHERE id = $1 ORDER BY "events"."id" LIMIT $2`)).
+					WithArgs(id, 1).
+					WillReturnError(gorm.ErrRecordNotFound)
+			},
+		},
+		{
+			name:    "failure find by id error",
+			success: false,
+			id:      event.EventID{UUID: uuid.New()},
+			setup: func(mock sqlmock.Sqlmock, id event.EventID) {
+				mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "events" WHERE id = $1 ORDER BY "events"."id" LIMIT $2`)).
+					WithArgs(id, 1).
+					WillReturnError(errors.New("find by id error"))
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			sqlDB, mock, err := sqlmock.New()
+			if err != nil {
+				t.Errorf("failed to new sqlmock: %s", err)
+			}
+
+			gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: sqlDB}), &gorm.Config{})
+			if err != nil {
+				t.Errorf("failed to open gorm: %s", err)
+			}
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			tt.setup(mock, tt.id)
+
+			repo := NewEventRepository(gormDB)
+
+			_, err = repo.FindByID(tt.id)
+			if tt.success && err != nil {
+				t.Errorf("expected no error, but got %v", err)
+			}
+			if !tt.success && err == nil {
+				t.Errorf("expected error, but got nil")
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
+
 func TestListByUserID(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
